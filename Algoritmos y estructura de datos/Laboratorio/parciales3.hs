2749af06342407dcd6f3befc7ba8028af8c5b190
Aike <aikebonetto78@gmail.com>
2024-09-24 09:03:38 -0300

    Add files via upload
    Colorin colorado, Haskell ha terminado

--- Algoritmos y estructura de datos/Laboratorio/parciales3.hs
+++ Algoritmos y estructura de datos/Laboratorio/parciales3.hs
@@ -29,9 +29,27 @@ x :: [Astro]
 x = [
     Planeta Rocoso 20 300,
     Planeta Gaseoso 30 220,
-    Estrella Gigante A "123"]
+    Estrella Gigante A "123"] 
 
-instance Ord Eq Astro
+{-
+instance Ord Astro
     where
-        ()
+        compare (Estrella O _ _) (Estrella B _ _) = compare O B 
+        compare (Estrella B _ _) (Estrella A _ _) = compare B A 
+        compare (Estrella A _ _) (Estrella F _ _) = compare A F 
+        compare (Estrella F _ _) (Estrella G _ _) = compare F G
+        compare (Estrella G _ _) (Estrella K _ _) = compare G K 
+        compare (Estrella K _ _) (Estrella M _ _) = compare K M
+
+estrellaIgual :: (Luminosidad Temperatura Nombre) -> (Luminosidad Temperatura Nombre) -> Bool 
+estrellaIgual (Estrella A t1 n1) (Estrella A t1' n1') = True 
+estrellaIgual (Estrella B t1 n1) (Estrella B t1' n1') = True 
+estrellaIgual (Estrella F t1 n1) (Estrella F t1' n1') = True 
+estrellaIgual (Estrella G t1 n1) (Estrella G t1' n1') = True 
+estrellaIgual (Estrella K t1 n1) (Estrella K t1' n1') = True 
+estrellaIgual (Estrella M t1 n1) (Estrella M t1' n1') = True 
+estrellaIgual (Estrella O t1 n1) (Estrella O t1' n1') = True 
+-}
+
+
 
--- Algoritmos y estructura de datos/Laboratorio/testFunciones.hs
+++ Algoritmos y estructura de datos/Laboratorio/testFunciones.hs
@@ -1,3 +1,20 @@
-varD :: [Int] -> Bool
-varD [] = False
-varD (x:y:xs) = (x == y + 1) && varD (y:xs)  
+type Titulo = String
+type Megusta = Int
+data Estado = Reproducido | SinReproducir
+    deriving Show
+type Duracion = Int
+data MovieList = SinMovie | Movie Titulo Megusta Estado Duracion MovieList
+    deriving Show
+
+condicionmegusta :: Megusta -> Bool
+condicionmegusta m  | m < 0 || m > 2 = False
+                    | m == 0 = True
+                    | m == 1 = True
+                    | m == 2 = True
+
+
+minutosReproducidos :: MovieList -> Megusta -> Duracion
+minutosReproducidos (Movie  a m1 b d resto) m2  | condicionmegusta (m1) = minutosReproducidos (Movie  a m1 b d resto) m2
+                                                | condicionmegusta (m2) = minutosReproducidos (Movie  a m1 b d resto) m2
+                                                | m1 >= m2 = d + minutosReproducidos resto m2
+                                                | otherwise = minutosReproducidos resto m2
